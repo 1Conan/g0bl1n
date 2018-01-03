05f9addc026fa561d2ab1f0bcf384579fe49bc2b
1Conan <1Conan@users.noreply.github.com>
2018-01-03 17:02:16 +0900

    Update kpp.h

--- g0blin/kpp.h
+++ g0blin/kpp.h
@@ -9,7 +9,7 @@
 
 #ifndef kpp_h
 #define kpp_h
-
+    
 #include <dlfcn.h>
 #include <copyfile.h>
 #include <stdio.h>
@@ -21,21 +21,21 @@
 #include <sys/mount.h>
 #include <sys/utsname.h>
 #include <Foundation/Foundation.h>
-
-
+        
+    
 kern_return_t do_kpp(int nukesb, int uref, uint64_t kernbase, uint64_t slide, task_t tfp0, uint64_t credpatch);
-
-
+    
+    
 size_t kread(uint64_t where, void *p, size_t size);
 uint64_t kread_uint64(uint64_t where);
 uint32_t kread_uint32(uint64_t where);
 size_t kwrite(uint64_t where, const void *p, size_t size);
 size_t kwrite_uint64(uint64_t where, uint64_t value);
 size_t kwrite_uint32(uint64_t where, uint32_t value);
-
+    
 void kx2(uint64_t fptr, uint64_t arg1, uint64_t arg2);
 uint32_t kx5(uint64_t fptr, uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4, uint64_t arg5);
-
+    
 struct mac_policy_ops {
     uint64_t mpo_audit_check_postselect;
     uint64_t mpo_audit_check_preselect;
@@ -373,19 +373,20 @@ struct mac_policy_ops {
     uint64_t mpo_iokit_check_filter_properties;
     uint64_t mpo_iokit_check_get_property;
 };
-
+    
 kern_return_t mach_vm_read_overwrite(vm_map_t target_task, mach_vm_address_t address, mach_vm_size_t size, mach_vm_address_t data, mach_vm_size_t *outsize);
 kern_return_t mach_vm_write(vm_map_t target_task, mach_vm_address_t address, vm_offset_t data, mach_msg_type_number_t dataCnt);
 kern_return_t mach_vm_protect(vm_map_t target_task, mach_vm_address_t address, mach_vm_size_t size, boolean_t set_maximum, vm_prot_t new_protection);
 kern_return_t mach_vm_allocate(vm_map_t target, mach_vm_address_t *address, mach_vm_size_t size, int flags);
-
+    
 #define ReadAnywhere32 kread_uint32
 #define WriteAnywhere32 kwrite_uint32
 #define ReadAnywhere64 kread_uint64
 #define WriteAnywhere64 kwrite_uint64
-
+    
 #define copyin(to, from, size) kread(from, to, size)
 #define copyout(to, from, size) kwrite(to, from, size)
-
-
+    
+    
 #endif /* kpp_h */
+    
